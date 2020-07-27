9bf3c0cfecf144dea5c2a6077d2ae98842ce0fba
GL553VD <GL553VD@FX553VD-DM585T>
2020-07-27 08:14:21 +0300

    ok8

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,14 @@ import 'MyLibrary/import_file.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
 const String GlobalPersonBoxLog = "GlobalPersonBoxLog";
+const String myAppID = "coolme";
+const String urld =
+    'mongodb://note8g2018:123456789@localhost:27017/flutter?authSource=admin';
 
 void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
-  //WidgetsBinding.instance.ensureVisualUpdate();
+  //await MongoStitchClient.initializeApp(urld);
   final Directory _directory = await path.getApplicationDocumentsDirectory();
   final String _hivePath = _directory.path;
   debugPrint(_hivePath);
--- lib/screen/Wall2_screen.dart
+++ lib/screen/Wall2_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
-
 import 'package:coolme121/model/Article_model.dart';
 
 import '../MyLibrary/import_file.dart';
-import 'package:mongo_dart/mongo_dart.dart' as mongo;
-import 'package:dio/dio.dart' as DIO;
+//import 'package:mongo_dart/mongo_dart.dart' as mongo;
+//import 'package:dio/dio.dart' as DIO;
 
 class Wall2 extends StatefulWidget 
 {
@@ -23,32 +21,40 @@ class _Wall2State extends State<Wall2>
   int time;
   int distance;
   //ScrollController _scrollController;
-  StreamController _streamController;
-  Stream _stream;
+  StreamController<Article> _streamController;
+  Stream<Article> _stream;
+  List<Article> articleList = [];
 
   void _getStreamArticle() async
   {
-    const String url = 'http://192.168.1.100:8888';
-//    DIO.RequestOptions option = DIO.RequestOptions();
-//    option.responseType = DIO.ResponseType.stream;
-//    option.baseUrl = url;
-////    option.contentType = DIO.Headers.jsonContentType;
-//    option.method = 'GET';
-//    //option.data = jsonEncode(article.toJson());
-//    DIO.Dio dio = DIO.Dio();
-//    DIO.Response response;
-//    response = await dio.request("/articleall", options: option);
-    String url2 = 'http://192.168.1.100:8888/articleall';
-    Response response = await get(url2);
-    _streamController.add(jsonDecode(response.body));
-    _stream.listen((event) {
-      print(event);
-    },
-      onDone: (){
-      print("Done");
-      }
-    );
+    const String url2 = 'http://192.168.1.100:8888/articleall';
+    const String url3 = 'http://192.168.1.100:3000/articleall';
+    const String url = 'http://192.168.1.100:8888/articleall';
+    const String url4 = 'http://192.168.1.100:3000';
 
+    // or get documents as they are recieved
+    final client = Client();
+    final streamedResponse = await client.send(Request('get', Uri.parse(url2)));
+    streamedResponse.stream.asBroadcastStream();
+    streamedResponse.stream.transform(Utf8Decoder())
+    .map((event) => jsonDecode(event) as Map<String, dynamic>)
+    .forEach((element) {
+      Article mArticle = Article.fromJson(element);
+      _streamController.add(mArticle);
+      articleList.add(mArticle);
+    });
+    _stream.forEach((element) {print(element.body);});
+
+//    final jsonStream = const Utf8Decoder()
+//        .bind(streamedResponse.stream)
+//        .map((str) => json.decode(str) as Map<String, dynamic>);
+//    await jsonStream.forEach((json) {
+//      assert(json is Map<String, dynamic>);
+//      articleList.add(Article.fromJson(json));
+//      print(json);
+//    });
+//
+//    client.close();
   }
 
   @override
@@ -57,7 +63,7 @@ class _Wall2State extends State<Wall2>
    // _scrollController = ScrollController();
     // TODO: implement initState
     super.initState();
-    _streamController = StreamController();
+    _streamController = StreamController<Article>();
     _stream = _streamController.stream;
     _getStreamArticle();
     //WidgetsBinding.instance.addPostFrameCallback((timeStamp) => jumpToBottom());
